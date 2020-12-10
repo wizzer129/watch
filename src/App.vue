8bda2d996cff51e7f3eeba7c010123712ddf0d82
Alejandro Lazo <alejandrolazo93@gmail.com>
2020-12-09 23:04:36 -0800

    :sparkles: Added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,108 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app style="-webkit-app-region: drag">
+        <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent color="#2C3A47" dark app>
+            <v-list-item class="px02 pt-1">
+                <v-list-item-avatar>
+                    <v-img src="./assets/watch.jpg" alt="admin" class="mx-auto" />
+                </v-list-item-avatar>
+                <v-list-item-title class="ml-4 text-capitalize">Lazo Ltd</v-list-item-title>
+            </v-list-item>
+            <v-list shaped class="clickable">
+                <template v-for="item in items">
+                    <v-list-group
+                        v-if="item.children"
+                        :key="item.text"
+                        v-model="item.model"
+                        :prepend-icon="item['icon-ctr']"
+                        :append-icon="item.mode ? item.icon : item['icon-alt']"
+                        active-class="orange--text"
+                    >
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{ item.text }}
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+                        <v-list-item
+                            v-for="(child, i) in item.children"
+                            :key="i"
+                            route
+                            :to="child.route"
+                            active-class="orange"
+                        >
+                            <v-list-item-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-item-action>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+                            <v-list-item-content>
+                                <v-list-item-tile>
+                                    {{ child.text }}
+                                </v-list-item-tile>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-group>
+                    <v-list-item v-else :key="item.text" active-class="orange--text" route :to="item.route">
+                        <v-list-item-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-title>
+                            {{ item.text }}
+                        </v-list-item-title>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
+    name: 'App',
+    props: {
+        source: String,
+    },
 
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        drawer: null,
+        mini: false,
+        fab: false,
+        items: [
+            { icon: 'mdi-home', text: 'Dashboard', route: '/' },
+            {
+                icon: 'mdi-chevron-up',
+                'icon-alt': 'mdi-chevron-down',
+                'icon-ctr': 'mdi-cart-arrow-right',
+                text: 'Orders',
+                model: false,
+                children: [
+                    { icon: 'style', text: 'Type', route: '/Type' },
+                    { icon: 'atm', text: 'Mark', route: '/Mark' },
+                ],
+                route: '/',
+            },
+            {
+                icon: 'mid-chevron-up',
+                'icon-alt': 'mdi-chevron-down',
+                'icon-ctr': 'mdi-google-maps',
+                text: 'Tracking',
+                model: false,
+                children: [
+                    { icon: 'mdi-tooltip-account', text: 'Locate', route: '/locate' },
+                    { icon: 'mdi-printer', text: 'Print', route: '/print' },
+                ],
+                route: '/',
+            },
+            { icon: 'mdi-finance', text: 'Revenue', route: '/revenue' },
+            { icon: 'mdi-chart-pie', text: 'Analytics', route: '/chart' },
+            { icon: 'mdi-magnify', text: 'Search', route: '/Recherche' },
+        ],
+    }),
 };
 </script>
+
+<style>
+.clickable {
+    -webkit-app-region: no-drag;
+}
+</style>
